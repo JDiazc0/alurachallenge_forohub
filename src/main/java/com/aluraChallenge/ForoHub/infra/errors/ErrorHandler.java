9829396b5a3ffd59a5ea92f7daae5e60e263b038
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-12-09 09:27:17 -0500

    feat update 400 Error

--- src/main/java/com/aluraChallenge/ForoHub/infra/errors/ErrorHandler.java
+++ src/main/java/com/aluraChallenge/ForoHub/infra/errors/ErrorHandler.java
@@ -6,19 +6,26 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 @RestControllerAdvice
 public class ErrorHandler {
     public record ErrorResponse(String error, String message){}
+    public record ValidationError(String field, String message){}
+    public record ValidationErrorResponse(String error, List<ValidationError> errors){};
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity Error404Handler(EntityNotFoundException e){
+    public ResponseEntity<ErrorResponse> Error404Handler(EntityNotFoundException e){
         var errorResponse = new ErrorResponse("Resource not found", e.getMessage());
         return ResponseEntity.status(404).body(errorResponse);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity Error400Handler(MethodArgumentNotValidException e){
-        var errors = e.getAllErrors();
-        return ResponseEntity.badRequest().body(errors);
+    public ResponseEntity<ValidationErrorResponse> Error400Handler(MethodArgumentNotValidException e){
+        var errors = e.getFieldErrors()
+                .stream()
+                .map(fieldError -> new ValidationError(fieldError.getField(), fieldError.getDefaultMessage()))
+                .toList();
+        return ResponseEntity.badRequest().body(new ValidationErrorResponse("Validation failed", errors));
     }
 }
