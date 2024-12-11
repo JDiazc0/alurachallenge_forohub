933b46eeaf7a3501e0f43924607ced6447a8f064
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-12-10 20:10:38 -0500

    feat create find by status func

--- src/main/java/com/aluraChallenge/ForoHub/Topic/Repository/TopicRepository.java
+++ src/main/java/com/aluraChallenge/ForoHub/Topic/Repository/TopicRepository.java
@@ -1,7 +1,11 @@
 package com.aluraChallenge.ForoHub.Topic.Repository;
 
+import com.aluraChallenge.ForoHub.Topic.Resources.TopicStatus;
 import com.aluraChallenge.ForoHub.Topic.Topic;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+    Page<Topic> findByStatus(TopicStatus topicStatus, Pageable pageable);
 }
